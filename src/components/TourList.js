702c10119150557f1a840e2e87a9f942449c6126
Karuma Lin <qscwdvefb3@gmail.com>
2022-08-23 09:01:30 +0800

    update

--- src/components/TourList.js
+++ src/components/TourList.js
@@ -51,152 +51,147 @@ const TourList = () => {
 
   return (
     <div className="container">
-      {!loading ?
-        <div className="form-floating m-3">
-          <button
-            type="button"
-            className="btn btn-outline-secondary btn-sm position-absolute top-50 end-0 translate-middle-y me-2"
-            onClick={() => {
-              if (!search) {
-                return;
-              }
-              setSearch('');
-              setTargetPage(1);
-            }}
-          >
-            X
-          </button>
-          <input
-            type="text"
-            className="form-control"
-            id="search"
-            placeholder="search"
-            value={search}
-            onChange={(e) => {
-              setSearch(e.target.value);
-              setTargetPage(1);
-            }}
-          />
-          <label
-            for="search"
-            style={{ display: 'block', 'text-align': 'left' }}
-          >
-            Search
-          </label>
-        </div>
-      : null}
-      <div className="row">
-        {loading ? <Loading /> :
-          data
-            .filter((item, i) => {
-              if (search === '') {
-                return true;
-              } else {
-                return item.Name.match(search);
-              }
-            })
-            .filter((item, i) => {
-              return Math.ceil((i + 1) / 20) === targetPage;
-            })
-            .map((item, i) => {
-              return (
-                <div key={i} className="col-md-6 py-2">
-                  <div className="card card-img-radius" title={item.Name}>
-                    <Link
-                      to={`/tour/${item.Id}`}
-                      state={{
-                        search: search,
-                        page: page,
-                        targetPage: targetPage,
-                      }}
-                    >
-                      <div className="card bg-black card-img-radius">
-                        <img
-                          src={item.Picture1}
-                          className="card-img-top img-cover card-img-radius"
-                          height="200px"
-                          alt={item.Name}
-                        />
-                      </div>
-                      <div className="card-img-overlay d-flex flex-column justify-content-around tourCard-img card-img-radius">
-                        <h4>{item.Name}</h4>
-                        <h6>[◉"] - {item.Picdescribe1} - [◉"]</h6>
-                      </div>
-                    </Link>
-                  </div>
-                </div>
-              );
-            })
-        }
-      </div>
-
-      <ul class="pagination justify-content-center my-4">
-        <li
-          className={targetPage === 1 ? 'disabled' : ''}
-          onClick={() => {
-            if (targetPage === 1) {
-              return;
-            } else {
-              setTargetPage(targetPage - 1);
-              window.scrollTo({ top: 0, behavior: 'smooth' });
-            }
-          }}
-        >
-          <a className="page-link" href="#/tour">
-            &lt;
-          </a>
-        </li>
-        {!loading
-          ? page.map((item, i) => {
-              return (
-                <li
-                  key={i}
-                  className={`d-none d-md-block ${
-                    item === targetPage ? 'active' : ''
-                  }`}
-                  onClick={() => {
-                    setTargetPage(item);
-                    window.scrollTo({ top: 0, behavior: 'smooth' });
-                  }}
-                >
-                  <a className="page-link" href="#/tour">
-                    {item}
-                  </a>
-                </li>
-              );
-            })
-          : null}
-        {!loading
-          ? page
+      {loading ? <Loading /> :
+        <>
+          <div className="form-floating m-3">
+            <button
+              type="button"
+              className="btn btn-outline-secondary btn-sm position-absolute top-50 end-0 translate-middle-y me-2"
+              onClick={() => {
+                if (!search) {
+                  return;
+                }
+                setSearch('');
+                setTargetPage(1);
+              }}
+            >
+              X
+            </button>
+            <input
+              type="text"
+              className="form-control"
+              id="search"
+              placeholder="search"
+              value={search}
+              onChange={(e) => {
+                setSearch(e.target.value);
+                setTargetPage(1);
+              }}
+            />
+            <label
+              for="search"
+              style={{ display: 'block', 'text-align': 'left' }}
+            >
+              Search
+            </label>
+          </div>
+        <div className="row">
+            {data
               .filter((item, i) => {
-                return item === targetPage;
+                if (search === '') {
+                  return true;
+                } else {
+                  return item.Name.match(search);
+                }
+              })
+              .filter((item, i) => {
+                return Math.ceil((i + 1) / 20) === targetPage;
               })
               .map((item, i) => {
                 return (
-                  <li key={i} className="d-block d-md-none">
-                    <a className="page-link" disabled href={() => false}>
+                  <div key={i} className="col-md-6 py-2">
+                    <div className="card card-img-radius" title={item.Name}>
+                      <Link
+                        to={`/tour/${item.Id}`}
+                        state={{
+                          search: search,
+                          page: page,
+                          targetPage: targetPage,
+                        }}
+                      >
+                        <div className="card bg-black card-img-radius">
+                          <img
+                            src={item.Picture1}
+                            className="card-img-top img-cover card-img-radius"
+                            height="200px"
+                            alt={item.Name}
+                          />
+                        </div>
+                        <div className="card-img-overlay d-flex flex-column justify-content-around tourCard-img card-img-radius">
+                          <h4>{item.Name}</h4>
+                          <h6>[◉"] - {item.Picdescribe1} - [◉"]</h6>
+                        </div>
+                      </Link>
+                    </div>
+                  </div>
+                );
+              })
+          }
+        </div>
+        <ul class="pagination justify-content-center my-4">
+          <li
+            className={targetPage === 1 ? 'disabled' : ''}
+            onClick={() => {
+              if (targetPage === 1) {
+                return;
+              } else {
+                setTargetPage(targetPage - 1);
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+              }
+            }}
+          >
+            <a className="page-link" href="#/tour">
+              &lt;
+            </a>
+          </li>
+            {page.map((item, i) => {
+                return (
+                  <li
+                    key={i}
+                    className={`d-none d-md-block ${
+                      item === targetPage ? 'active' : ''
+                    }`}
+                    onClick={() => {
+                      setTargetPage(item);
+                      window.scrollTo({ top: 0, behavior: 'smooth' });
+                    }}
+                  >
+                    <a className="page-link" href="#/tour">
                       {item}
                     </a>
                   </li>
                 );
-              })
-          : null}
-        <li
-          className={targetPage === page.length ? 'disabled' : ''}
-          onClick={() => {
-            if (targetPage === page.length) {
-              return;
-            } else {
-              setTargetPage(targetPage + 1);
-              window.scrollTo({ top: 0, behavior: 'smooth' });
-            }
-          }}
-        >
-          <a className="page-link" href="#/tour">
-            &gt;
-          </a>
-        </li>
-      </ul>
+              })}
+            {page.filter((item, i) => {
+                  return item === targetPage;
+                })
+                .map((item, i) => {
+                  return (
+                    <li key={i} className="d-block d-md-none">
+                      <a className="page-link" disabled href={() => false}>
+                        {item}
+                      </a>
+                    </li>
+                  );
+                })}
+          <li
+            className={targetPage === page.length ? 'disabled' : ''}
+            onClick={() => {
+              if (targetPage === page.length) {
+                return;
+              } else {
+                setTargetPage(targetPage + 1);
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+              }
+            }}
+          >
+            <a className="page-link" href="#/tour">
+              &gt;
+            </a>
+          </li>
+        </ul>
+      </>
+      }
     </div>
   );
 }
